9460a3fca17e848bc1cfea6c00b226c345b5eb0b
Jonah Witcig <jwitcig@gmail.com>
2019-02-18 20:55:06 -0600

    Separated resources from repo.

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   pixelSkipCount: 5,
   blue: { r: 0, g: 63, b: 255, a: 255 },
   // blue: { r: 0, g: 185, b: 185, a: 185 },
-  imagePath: './lake.png',
+  imagePath: '../map-resources/lake.png',
   routeImagePath: './path.png',
 };
